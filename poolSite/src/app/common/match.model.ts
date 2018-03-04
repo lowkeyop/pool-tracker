a62538cca9ddf0503f926acbf488ab7c320e6a99
lowkeyop <ckennerly9@gmail.com>
2018-03-04 02:21:40 -0500

    Moved functionality to Model and added more methods
    Moved some methods from scoresheet component to the Match Model to make
    code cleaner.  Added function in Match model to determine:
    determineWinPlayerWinGoal
    takenTimeOutsForPlayer
    countTotalInnings
    eightOnBreaksForPlayer
    breakNRunsForPlayer
    determineGamesWon
    determinePointsEarned

--- poolSite/src/app/common/match.model.ts
+++ poolSite/src/app/common/match.model.ts
@@ -6,84 +6,156 @@ export class Match {
   constructor(
     public homeTeamPlayer: Player,
     public awayTeamPlayer: Player,
+
+    public homeTeamWinGoal: number,
+    public awayTeamWinGoal: number,
+
+
     public winner: Player,
     public games: Game[],
     public totalHomeTeamTimeoutsTaken: number=0,
     public totalAwayTeamTimeoutsTaken: number=0,
-    public homeTeamPlayerTotalDefensiveShots: number=0,
-    public awayTeamPlayerTotalDefensiveShots: number=0,
-    public homeTeamPlayerPointsEarned: number=0,
-    public awayTeamPlayerPointsEarned: number=0,
+    public totalInnings: number=0,
+
+    public awayTeamPlayerEightOnBreak: number=0,
+    public homeTeamPlayerEightOnBreak: number=0,
+
     public awayTeamPlayerBreakAndRuns: number=0,
     public homeTeamPlayerBreakAndRuns: number=0,
-    public awayTeamPlayerEightOnBreak: number=0,
-    public homeTeamPlayerEightOnBreak: number=0
+
+    public homeTeamPlayerTotalDefensiveShots: number=0,
+    public awayTeamPlayerTotalDefensiveShots: number=0,
+
+    public homeTeamPlayerGamesWon: number=0,
+    public awayTeamPlayerGamesWon: number=0,
+
+    public homeTeamPlayerPointsEarned: number=0,
+    public awayTeamPlayerPointsEarned: number=0
   ){}
+
+
+  determineWinPlayerWinGoal(player: Player, opp: Player){
+    var tSkillLevel = player.playerSkillLevel;
+    var oSkillLevel = opp.playerSkillLevel;
+    var winGoal = 0;
+    if (tSkillLevel == 2 || oSkillLevel == 2) {
+				winGoal = tSkillLevel;
+			} else if (tSkillLevel > 2) {
+				winGoal = tSkillLevel - 1;
+			} else if (tSkillLevel == 7 && (oSkillLevel > 3)) {
+				winGoal = tSkillLevel - 2;
+			} else if (tSkillLevel == 7 && oSkillLevel == 2) {
+				winGoal = tSkillLevel;
+			}
+      return winGoal;
+  }
   takenTimeOutsForPlayer(player: Player){
     var timeoutsTaken :number =0;
     for(let game of this.games){
       if(player == this.homeTeamPlayer){
-        timeoutsTaken+=this.game.p1TimeoutsTaken;
+        timeoutsTaken+=game.p1TimeoutsTaken;
       }
       if(player == this.awayTeamPlayer){
-        timeoutsTaken+=this.game.p2TimeoutsTaken;
+        timeoutsTaken+=game.p2TimeoutsTaken;
       }
     }
     return timeoutsTaken;
   }
-  BreakNRunsForPlayer(player: Player){
-    var totalBreakNRuns :number =0;
-    for(let game of this.games){
-      if(player == this.homeTeamPlayer && this.game.bnr && this.game.winner == player){
-        totalBreakNRuns++;
-      }
-      if(player == this.awayTeamPlayer && this.game.bnr && this.game.winner == player){
-        totalBreakNRuns++;
-      }
-    }
-    return totalBreakNRuns;
-  }
-  EightOnBreaksForPlayer(player: Player){
-    var totalEightOnBreaks :number =0;
-    for(let game of this.games){
-      if(player == this.homeTeamPlayer && this.game.bnr && this.game.winner == player){
-        totalEightOnBreaks++;
-      }
-      if(player == this.awayTeamPlayer && this.game.bnr && this.game.winner == player){
-        totalEightOnBreaks++;
-      }
-    }
-    return totalEightOnBreaks;
-  }
-  countInnings(){
+  countTotalInnings(){
     var count =0;
     for(let game of this.games){
       count+=game.innings;
     }
     return count;
   }
+  eightOnBreaksForPlayer(player: Player){
+    var totalEightOnBreaks :number =0;
+    for(let game of this.games){
+      if(player == this.homeTeamPlayer && game.bnr && game.winner == player){
+        totalEightOnBreaks++;
+      }
+      if(player == this.awayTeamPlayer && game.bnr && game.winner == player){
+        totalEightOnBreaks++;
+      }
+    }
+    return totalEightOnBreaks;
+  }
+  breakNRunsForPlayer(player: Player){
+    var totalBreakNRuns :number =0;
+    for(let game of this.games){
+      if(player == this.homeTeamPlayer && game.bnr && game.winner == player){
+        totalBreakNRuns++;
+      }
+      if(player == this.awayTeamPlayer && game.bnr && game.winner == player){
+        totalBreakNRuns++;
+      }
+    }
+    return totalBreakNRuns;
+  }
+  determineGamesWon(player: Player){
+    var winCount = 0;
+    for(let game of this.games){
+      if(game.winner == player){
+        winCount++;
+      }
+    }
+    return winCount;
+  }
+  determinePointsEarned(player: Player){
+    var winGoal: number;
+    var totalWins: number;
+    var pointsEarned: number;
+    var opponent: Player;
+    var oppMadeItToHill: boolean
+    var madeItToHill: boolean;
+
+    if(player == this.homeTeamPlayer && player == this.winner){
+      winGoal = this.determineWinPlayerWinGoal(this.homeTeamPlayer, this.awayTeamPlayer);
+      totalWins = this.determineGamesWon(this.homeTeamPlayer);
+      opponent = this.awayTeamPlayer;
+    } else if(player == this.homeTeamPlayer && player == this.winner){
+      winGoal = this.determineWinPlayerWinGoal(this.awayTeamPlayer, this.homeTeamPlayer);
+      totalWins = this.determineGamesWon(this.awayTeamPlayer);
+      opponent = this.homeTeamPlayer;
+    }
+    oppMadeItToHill = this.determineWinPlayerWinGoal(opponent, player) - this.determineGamesWon(opponent) == 1;
+    madeItToHill = this.determineWinPlayerWinGoal(player, opponent) - this.determineGamesWon(player) == 1;
+    var isShutout = totalWins = 0;
+    var wonAGame = totalWins > 0;
+    if(isShutout){
+      pointsEarned = 0;
+    }
+    if(player != this.winner && madeItToHill){
+      pointsEarned = 1;
+    }
+    if(player == this.winner && oppMadeItToHill){
+      pointsEarned = 2;
+    }
+    else
+      pointsEarned = 3;
+  }
+
   summarizeMatchInfo(){
-    var homeTeamPlayerTOs: number;
-    var awayTeamPlayerTOs: number;
 
-    var homeTeamBNRs: number;
-    var awayTeamBNRs: number;
+    var hWinGoal = this.determineWinPlayerWinGoal(this.homeTeamPlayer, this.awayTeamPlayer);
+    var aWinGoal = this.determineWinPlayerWinGoal(this.awayTeamPlayer, this.homeTeamPlayer);
 
-    var homeTeamEOBs: number;
-    var awayTeamEOBs: number;
+    var homeTeamPlayerTOs = this.takenTimeOutsForPlayer(this.homeTeamPlayer);
+    var awayTeamPlayerTOs = this.takenTimeOutsForPlayer(this.awayTeamPlayer);
 
-    var totalInnings: number;
+    var totalInnings = this.countTotalInnings();
 
-    homeTeamPlayerTOs = this.takenTimeOutsForPlayer(this.homeTeamPlayer);
-    awayTeamPlayerTOs = this.takenTimeOutsForPlayer(this.awayTeamPlayer);
+    var homeTeamEOBs = this.eightOnBreaksForPlayer(this.homeTeamPlayer);
+    var awayTeamEOBs = this.eightOnBreaksForPlayer(this.awayTeamPlayer);
 
-    homeTeamBNRs = this.BreakNRunsForPlayer(this.homeTeamPlayer);
-    awayTeamBNRs = this.BreakNRunsForPlayer(this.awayTeamPlayer);
+    var homeTeamBNRs = this.breakNRunsForPlayer(this.homeTeamPlayer);
+    var awayTeamBNRs = this.breakNRunsForPlayer(this.awayTeamPlayer);
 
-    homeTeamEOBs = this.EightOnBreaksForPlayer(this.homeTeamPlayer);
-    awayTeamEOBs = this.EightOnBreaksForPlayer(this.awayTeamPlayer);
+    var hTDefShotsTotal : number;
+    var aTDefShotsTotal : number;
 
-    totalInnings = this.countInnings();
+    var htGamesWon = this.determineGamesWon(this.homeTeamPlayer);
+    var atGamesWon = this.determineGamesWon(this.awayTeamPlayer);
 
   }
 }
--- poolSite/src/app/score-sheet/score-sheet.component.ts
+++ poolSite/src/app/score-sheet/score-sheet.component.ts
@@ -82,73 +82,7 @@ export class ScoreSheetComponent implements OnInit {
           console.log("All Matches done for the night");
         }
   }
-  takenTimeOutsForPlayer(player: Player){
-    var timeoutsTaken :number =0;
-    for(let game of this.games){
-      if(player == this.homeTeamPlayer){
-        timeoutsTaken+=this.game.p1TimeoutsTaken;
-      }
-      if(player == this.awayTeamPlayer){
-        timeoutsTaken+=this.game.p2TimeoutsTaken;
-      }
-    }
-    return timeoutsTaken;
-  }
-  BreakNRunsForPlayer(player: Player){
-    var totalBreakNRuns :number =0;
-    for(let game of this.games){
-      if(player == this.homeTeamPlayer && this.game.bnr && this.game.winner == player){
-        totalBreakNRuns++;
-      }
-      if(player == this.awayTeamPlayer && this.game.bnr && this.game.winner == player){
-        totalBreakNRuns++;
-      }
-    }
-    return totalBreakNRuns;
-  }
-  EightOnBreaksForPlayer(player: Player){
-    var totalEightOnBreaks :number =0;
-    for(let game of this.games){
-      if(player == this.homeTeamPlayer && this.game.bnr && this.game.winner == player){
-        totalEightOnBreaks++;
-      }
-      if(player == this.awayTeamPlayer && this.game.bnr && this.game.winner == player){
-        totalEightOnBreaks++;
-      }
-    }
-    return totalEightOnBreaks;
-  }
-  countInnings(){
-    var count =0;
-    for(let game of this.games){
-      count+=game.innings;
-    }
-    return count;
-  }
-  summarizeMatchInfo(){
-    var homeTeamPlayerTOs: number;
-    var awayTeamPlayerTOs: number;
 
-    var homeTeamBNRs: number;
-    var awayTeamBNRs: number;
-
-    var homeTeamEOBs: number;
-    var awayTeamEOBs: number;
-
-    var totalInnings: number;
-
-    homeTeamPlayerTOs = this.takenTimeOutsForPlayer(this.homeTeamPlayer);
-    awayTeamPlayerTOs = this.takenTimeOutsForPlayer(this.awayTeamPlayer);
-
-    homeTeamBNRs = this.BreakNRunsForPlayer(this.homeTeamPlayer);
-    awayTeamBNRs = this.BreakNRunsForPlayer(this.awayTeamPlayer);
-
-    homeTeamEOBs = this.EightOnBreaksForPlayer(this.homeTeamPlayer);
-    awayTeamEOBs = this.EightOnBreaksForPlayer(this.awayTeamPlayer);
-
-    totalInnings = this.countInnings();
-
-  }
   endCurrentMatch(){
     this.isMatchStarted = false;
     this.areBothTeamsReady = false;
