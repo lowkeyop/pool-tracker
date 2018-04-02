df39aa6db9110880eb8466f051cf1bc90c7ba333
lowkeyop <ckennerly9@gmail.com>
2018-04-01 22:51:33 -0400

    Creating a Single Page Application Style
    Added styling and functionality for SPA functionality.
    Changed the view setup for  match-results component.

--- poolSite/src/app/header/header.component.ts
+++ poolSite/src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -9,6 +9,40 @@ export class HeaderComponent implements OnInit {
 
   constructor() { }
 
+  showMatchView : boolean = true;
+  showTableDistributeView : boolean = false;
+  showAdministrativeView : boolean = false;
+  @Output() selectedView: EventEmitter<string> = new EventEmitter<string>();
+
+  toggleView( viewName : string){
+    switch(viewName){
+      case "match" : {
+        this.showMatchView = true;
+        this.showTableDistributeView = false;
+        this.showAdministrativeView  = false;
+        this.selectedView.emit(viewName);
+        break;
+      }
+      case "tables" : {
+        this.showMatchView = false;
+        this.showTableDistributeView = true;
+        this.showAdministrativeView  = false;
+        this.selectedView.emit(viewName);
+        break;
+      }
+      case "admin" : {
+        this.showMatchView = false;
+        this.showTableDistributeView = false;
+        this.showAdministrativeView  = true;
+        this.selectedView.emit(viewName);
+        break;
+      }
+      default :{
+        break
+      }
+    }
+
+  }
   ngOnInit() {
   }
 
